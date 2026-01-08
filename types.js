7835f6133d53a9f3d3b1624141c773b0b03ad932
Edrian Hernandez <117151322+EdrianHernandez@users.noreply.github.com>
2026-01-08 18:51:09 +0800

    Update and rename types.ts to types.js

---
+++ types.js
@@ -0,0 +1,14 @@
+/**
+ * @typedef {Object} Ingredient
+ * @property {string} item
+ * @property {string} amount
+ * @property {string} id
+ */
+
+export const Difficulty = {
+  Easy: 'Easy',
+  Medium: 'Medium',
+  Hard: 'Hard'
+};
+
+// Types removed: NutritionInfo, CookingStep, Recipe
